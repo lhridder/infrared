bd5095bc34fe7b264cf222aa142bcf827f56255e
lhridder <lhridder@gmail.com>
2022-06-08 19:30:50 +0200

    simplify EncryptionResponse and rename EncryptedSecretKey to SharedSecret

--- protocol/login/serverbound_encryptionresponse.go
+++ protocol/login/serverbound_encryptionresponse.go
@@ -12,11 +12,11 @@ type ServerBoundEncryptionResponse struct {
 }
 
 type ServerBoundEncryptionResponseNew struct {
-	EncryptedSecretKey protocol.ByteArray
-	UseVerifyToken     protocol.Boolean
-	VerifyToken        protocol.ByteArray
-	Salt               protocol.Long
-	Signature          protocol.ByteArray
+	SharedSecret   protocol.ByteArray
+	UseVerifyToken protocol.Boolean
+	VerifyToken    protocol.ByteArray
+	Salt           protocol.Long
+	Signature      protocol.ByteArray
 }
 
 func (pk ServerBoundEncryptionResponse) Marshal() protocol.Packet {
@@ -41,31 +41,16 @@ func UnmarshalServerBoundEncryptionResponse(packet protocol.Packet) (ServerBound
 	)
 	if err != nil {
 		err = packet.Scan(
-			&pknew.EncryptedSecretKey,
+			&pknew.SharedSecret,
 			&pknew.UseVerifyToken,
+			&pknew.VerifyToken,
+			&pknew.Salt,
+			&pknew.Signature,
 		)
 		if err != nil {
 			return pk, pknew, err
 		}
-		if pknew.UseVerifyToken {
-			err = packet.Scan(
-				&pknew.VerifyToken,
-				&pknew.UseVerifyToken,
-			)
-			if err != nil {
-				return pk, pknew, err
-			}
-			return pk, pknew, nil
-		} else {
-			err = packet.Scan(
-				&pknew.Salt,
-				&pknew.Signature,
-			)
-			if err != nil {
-				return pk, pknew, err
-			}
-			return pk, pknew, nil
-		}
+		return pk, pknew, nil
 	}
 
 	return pk, pknew, nil
